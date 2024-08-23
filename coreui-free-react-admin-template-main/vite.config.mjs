68aa0d1ed39de46161db35b92b580766d3bc8d12
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-08-23 15:13:52 +0100

    "last udpdate"

--- coreui-free-react-admin-template-main/vite.config.mjs
+++ coreui-free-react-admin-template-main/vite.config.mjs
@@ -40,7 +40,7 @@ export default defineConfig(() => {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss','jsx','css'],
     },
     server: {
-      port: 3000,
+      port: 3002,
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },
